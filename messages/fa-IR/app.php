68427354609b838d3d5be8f44bae74c419b4bcb4
Akrez <akrez.like@gmail.com>
2021-09-25 21:49:18 +0330

    msg

--- messages/fa-IR/app.php
+++ messages/fa-IR/app.php
@@ -13,4 +13,17 @@ return [
     'RelationRawDefault' => 'مواد خام مرتبط',
     'Information has been updated successfully.' => 'اطلاعات با موفقیت ویرایش شد.',
     'Information has been saved successfully.' => 'اطلاعات با موفقیت ذخیره شد.',
+    //
+    'alertRemoveDanger' => '{count} {child} مرتبط با این {parent} وجود دارد.',
+    'alertRemoveSuccessfull' => 'اطلاعات با موفقیت حذف شد.',
+    'alertRemoveUnSuccessfull' => 'حذف اطلاعات با خطا مواجه شد.',
+    'alertAddSuccessfull' => 'اطلاعات با موفقیت ایجاد شد.',
+    'alertUpdateSuccessfull' => 'اطلاعات با موفقیت ویرایش شد.',
+    'alertGalleryUploadSuccessfull' => 'عکس با موفقیت بارگذاری شد.',
+    'alertSignupSuccessfull' => 'ثبت نام با موفقیت انجام شد، لطفا وارد شوید.',
+    'alertResetPasswordRequestSuccessfull' => 'کد بازیابی رمز عبور برای شما ارسال شد.',
+    'alertResetPasswordSuccessfull' => 'بازیابی رمز عبور با موفقیت انجام شد، لطفا وارد شوید.',
+    'alertVerifyRequestSuccessfull' => 'کد فعال‌سازی برای شما ارسال شد.',
+    'alertVerifySuccessfull' => 'فعال‌سازی با موفقیت انجام شد.',
+    //
 ];

85426d7bdadd7fed2819f310a64d31ec19a630b1
Akrez <akrez.like@gmail.com>
2021-09-27 21:48:44 +0330

    TypeController

---
+++ controllers/TypeController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Type;
+use app\models\TypeSearch;
+use Yii;
+
+class TypeController extends Controller
+{
+
+    public static $category = Type::CATEGORY_RESELLER;
+
+    public function init()
+    {
+        parent::init();
+        $this->newModel = new Type([
+            'categoryId' => self::$category,
+        ]);
+        $this->searchModel = new TypeSearch([
+            'categoryId' => self::$category,
+        ]);
+    }
+
+    public function behaviors()
+    {
+        return $this->defaultBehaviors([
+            [
+                'actions' => ['index', 'delete', 'suggest'],
+                'allow' => true,
+                'verbs' => ['POST', 'GET'],
+                'roles' => ['@'],
+            ],
+        ]);
+    }
+
+    public function actionIndex($id = null)
+    {
+        return $this->commonIndex($id, [
+            'view' => '/type/index',
+            'extraParams' => [
+                'categoryId' => self::$category,
+            ],
+            'staticAttributes' => [
+                'categoryId' => self::$category,
+            ],
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        return $this->commonDelete($id);
+    }
+
+    public function actionSuggest()
+    {
+        $term = Yii::$app->request->get('term');
+        $results = [];
+        $databaseResults = Type::find()
+            ->where(['categoryId' => Type::CATEGORY_RESELLER])
+            ->andFilterWhere([
+                'OR',
+                ['LIKE', 'name', $term],
+                ['LIKE', 'shortname', $term],
+            ])
+            ->with('parent')->all();
+        foreach ($databaseResults as $databaseResult) {
+            $results[] = ['id' => $databaseResult->id, 'text' => $databaseResult->name . ' (' . $databaseResult->shortname . ')'];
+        }
+        return $this->asJson(['results' => $results]);
+    }
+}

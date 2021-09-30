0ce5419578d529f646002afb88eb7fc9cf094a7c
Akrez <akrez.like@gmail.com>
2021-09-30 21:33:58 +0330

    RawtypeController

--- controllers/RawtypeController.php
+++ controllers/RawtypeController.php
@@ -2,41 +2,63 @@
 
 namespace app\controllers;
 
-use app\models\RawType;
-use app\models\RawTypeSearch;
-use app\models\Type;
-use app\models\TypeSearch;
 use yii\helpers\Url;
+use app\models\RawType;
+use app\components\Helper;
+use app\controllers\Controller;
+use app\models\RawTypeSearch;
+use Yii;
 
 class RawtypeController extends Controller
 {
 
-    public function init()
-    {
-        parent::init();
-        $this->newModel = new RawType();
-        $this->searchModel = new RawTypeSearch();
-        $this->parentModel = new Type();
-        $this->parentSearchModel = new TypeSearch();
-    }
-
     public function behaviors()
     {
         return $this->defaultBehaviors([
-                    [
-                        'actions' => ['index', 'delete'],
-                        'allow' => true,
-                        'verbs' => ['POST', 'GET'],
-                        'roles' => ['@'],
-                    ],
+            [
+                'actions' => ['index', 'delete'],
+                'allow' => true,
+                'verbs' => ['POST', 'GET'],
+                'roles' => ['@'],
+            ],
         ]);
     }
 
     public function actionIndex($typeId, $id = null)
     {
-        $this->findParentModel($typeId);
-        $this->newModel->typeId = $typeId;
-        return $this->commonIndex($id);
+        $categoryClass = RawType::class;
+        $id = empty($id) ? null : intval($id);
+        $post = Yii::$app->request->post();
+        $state = Yii::$app->request->get('state', '');
+        $updateCacheNeeded = null;
+        //
+        if ($id) {
+            $model = Helper::findOrFail($categoryClass::validQuery()->andWhere(['id' => $id]));
+        } else {
+            $model = null;
+        }
+        $newModel = new $categoryClass();
+        $searchModel = new RawTypeSearch();
+        //
+        if ($state == 'save' && $newModel->load($post)) {
+            $updateCacheNeeded = Helper::store($newModel, $post, [
+                'categoryId' => $categoryClass,
+            ]);
+        } elseif ($state == 'update' && $model) {
+            $updateCacheNeeded = Helper::store($model, $post, [
+                'categoryId' => $categoryClass,
+            ]);
+        } elseif ($state == 'remove' && $model) {
+            $updateCacheNeeded = Helper::delete($model);
+        }
+        if ($updateCacheNeeded) {
+            $newModel = new $categoryClass();
+        }
+        //
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams, $categoryClass);
+        return $this->render('index', [
+            'state' => $state,
+        ] + compact('newModel', 'searchModel', 'model', 'dataProvider'));
     }
 
     public function actionDelete($id, $typeId)
@@ -45,5 +67,4 @@ class RawtypeController extends Controller
         $redirectUrl = Url::to(['/rawtype/index', 'typeId' => $typeId]);
         return $this->commonDelete($id, $redirectUrl);
     }
-
 }
